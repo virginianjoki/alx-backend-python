8ed0bad4c4e337c3b2cd650cffd8fda0766c5849
virginianjoki <njokivirginia0619@gmail.com>
2025-07-20 17:47:26 +0300

    messaging_app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
-from rest_framework import routers
+# from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 from rest_framework_nested.routers import NestedDefaultRouter
-
+from rest_framework_nested import routers
 router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+convo_router = routers.NestedDefaultRouter(
+    router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet,
+                      basename='conversation-messages')
+
+# router = routers.DefaultRouter()
+# router.register(r'conversations', ConversationViewSet, basename='conversation')
+# router.register(r'messages', MessageViewSet, basename='message')
 
 
 conversation_router = NestedDefaultRouter(
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,12 +19,14 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+# from rest_framework_nested import routers
+from rest_framework_nested import routers
+
 from chats.views import ConversationViewSet, MessageViewSet
 
-
 router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'conversations', ConversationViewSet,
+                basename='conversations')
 
 convo_router = routers.NestedDefaultRouter(
     router, r'conversations', lookup='conversation')

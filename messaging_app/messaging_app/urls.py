f123620ccf665c66b6e5dc5309463c84ee24a74a
virginianjoki <njokivirginia0619@gmail.com>
2025-07-20 17:34:06 +0300

    messaging_app

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -39,4 +39,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
     path('api/', include(convo_router.urls)),
+    path('api-auth/', include('rest_framework.urls')),  #
 ]

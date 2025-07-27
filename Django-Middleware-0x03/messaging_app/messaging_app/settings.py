f49e34d3986089df17b6f0e10113677d26338bcc
virginianjoki <njokivirginia0619@gmail.com>
2025-07-27 16:24:51 +0300

    add

--- Django-Middleware-0x03/messaging_app/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/messaging_app/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
